224893fa58d7f5018f158c151e8c4e32fe8b65f0
Patorikku0 <patrickaxsova@gmail.com>
2023-02-16 01:55:11 +0000

    i need continue

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -32,19 +32,28 @@ $(function () {
 createPlatform(100,100,20,500)
 createPlatform(0,800,10000,20) 
 createPlatform(100,600,900,20)
-createPlatform(100,450,500,20)
-    
+createPlatform(100,485,500,20)
+createPlatform(900,485,70,20)
+createPlatform(1250,640,200,20)
+createPlatform(1000, 400,50,20)
+createPlatform(1300, 330, 50, 20)
+createPlatform(700, 250, 50, 20)
+createPlatform(600,300,20,205)
+createPlatform(300,0,20,400)
+createPlatform(450,90,20,400)
+
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-createCollectable("steve", 1300, 620)
-createCollectable("grace", 10, 10)
-createCollectable("diamond",20, 20)
+createCollectable("database",20, 40, 0)
+createCollectable("steve", 1300, 600,0)
+createCollectable("grace", 920, 430, 0)
+createCollectable("diamond",100, 650, 0)
 createCollectable("max",30, 30)
-createCollectable("database",40, 40)
-createCollectable("kennedi",50, 50)
+createCollectable("kennedi",150,530, 0)
     
     
     // TODO 3
@@ -53,9 +62,9 @@ createCollectable("kennedi",50, 50)
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay)
 createCannon("left" ,300,1010)
-createCannon("right" ,800 ,1050)
-createCannon("top" ,600 ,1050)
-createCannon("bottom" ,300,1050)
+createCannon("right" ,800 ,1010)
+createCannon("top" ,600,1100)
+createCannon("bottom" ,300,1100)
 
 
     /////////////////////////////////////////////////
